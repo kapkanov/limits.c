8bf4f3c8c07e0f842764e957097403769be7db8a
Anatoly Polivin <anatoly@kapkanov.com>
2024-10-16 16:32:03 +0400

    add limits32 and limits64 with assertions

---
+++ limits32.c
@@ -0,0 +1,61 @@
+#ifndef LIMITS32_C
+#define LIMITS32_C
+
+#include "assert.c"
+#include "types32.c"
+
+const i8  I8_MIN  = -128;
+const i8  I8_MAX  =  127;
+const u8  U8_MAX  =  255;
+
+const i16 I16_MIN = -32768;
+const i16 I16_MAX =  32767;
+const u16 U16_MAX =  65535;
+
+const i32 I32_MIN = -2147483648;
+const i32 I32_MAX =  2147483647;
+const u32 U32_MAX =  4294967295;
+
+void assert_limits32(void) {
+  const u8 max8_unsigned = 0xff;
+  assert(U8_MAX == max8_unsigned, "U8_MAX: expected %lu but got %lu\n", max8_unsigned, U8_MAX);
+     
+  const i8 min8_signed = 0x80;
+  assert(I8_MIN == min8_signed, "I8_MIN: expected %d but got %d\n", min8_signed, I8_MIN);
+
+  const i8 max8_signed = 0x7f;
+  assert(I8_MAX == max8_signed, "I8_MAX: expected %d but got %d\n", max8_signed, I8_MAX);
+
+  const u16 max16_unsigned = 0xffff;
+  assert(U16_MAX == max16_unsigned, "U16_MAX: expected %d but got %d\n", max16_unsigned, U16_MAX);
+
+  const i16 min16_signed = 0x8000;
+  assert(I16_MIN == min16_signed, "I16_MIN: expected %d but got %d\n", min16_signed, I16_MIN);
+
+  const i16 max16_signed = 0x7fff;
+  assert(I16_MAX == max16_signed, "I16_MAX: expected %d but got %d\n", max16_signed, I16_MAX);
+
+  /*
+   * 2^32 - 1 = 2 * 2^31 - 1 = 2^31 - 1 + 2^31 = 2^30 * 2 - 1 + 2^30 * 2
+   */
+  const u32 two_pow_thirty_unsigned = 1024 * 1024 * 1024;
+  u32       max32_unsigned;
+  max32_unsigned  = 2 * two_pow_thirty_unsigned;
+  max32_unsigned -= 1;
+  max32_unsigned += 2 * two_pow_thirty_unsigned;
+  assert(U32_MAX == max32_unsigned, "U32_MAX: expected %lu but got %lu\n", max32_unsigned, U32_MAX);
+  assert(0xffffffff == U32_MAX, "U32_MAX in binary: expected %lu but got %lu\n", 0xffffffff, U32_MAX);
+  
+  /*
+   * 2^31 - 1 = 2 * 2^30 - 1 = 2^30 - 1 + 2^30
+   */
+  const i32 two_pow_thirty_signed = 1024 * 1024 * 1024;
+  i32       max32_signed;
+  max32_signed  = two_pow_thirty_signed;
+  max32_signed -= 1;
+  max32_signed += two_pow_thirty_signed;
+  assert(I32_MAX == max32_signed, "I32_MAX: expected %d but got %d\n", max32_signed, I32_MAX);
+  assert(0x7fffffff == I32_MAX, "I32_MAX in binary: expected %d but got %d\n", 0x7fffffff, I32_MAX);
+}
+
+#endif
---
+++ limits64.c
@@ -0,0 +1,36 @@
+#ifndef LIMITS64_C
+#define LIMITS64_C
+
+#include "assert.c"
+#include "types64.c"
+
+const i64 I64_MIN = 0x8000000000000000; /* -9223372036854775808 */
+const i64 I64_MAX = 0x7fffffffffffffff; /*  9223372036854775807 */
+const u64 U64_MAX = 0xffffffffffffffff; /* 18446744073709551615 */
+
+void assert_limits64(void) {
+  /*
+   * 2^64 - 1 = 2 * 2^63 - 1 = 2^63 - 1 + 2^63 = 2^60 * 8 - 1 + 2^60 * 8
+   */
+  const u64 two_pow_thirty_unsigned = 1024 * 1024 * 1024;
+  const u64 two_pow_sixty_unsigned  = two_pow_thirty_unsigned * two_pow_thirty_unsigned;
+  u64       max64_unsigned;
+  max64_unsigned  = two_pow_sixty_unsigned;
+  max64_unsigned  = 8 * two_pow_sixty_unsigned;
+  max64_unsigned -= 1;
+  max64_unsigned += 8 * two_pow_sixty_unsigned;
+  assert(U64_MAX == max64_unsigned, "U64_MAX: expected %llu but got %llu\n", max64_unsigned, U64_MAX);
+  
+  /*
+   * 2^63 - 1 = 8 * 2^60 - 1 = 4 * 2^60 - 1 + 4 * 2^60
+   */
+  const i64 two_pow_thirty_signed = 1024 * 1024 * 1024;
+  const i64 two_pow_sixty_signed  = two_pow_thirty_signed * two_pow_thirty_signed;
+  i64       max64_signed;
+  max64_signed  = 4 * two_pow_sixty_signed;
+  max64_signed -= 1;
+  max64_signed += 4 * two_pow_sixty_signed;
+  assert(I64_MAX == max64_signed, "I64_MAX: expected %lld but got %lld\n", max64_signed, I64_MAX);
+}
+
+#endif
