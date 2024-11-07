6359b8697821d3063e0a5d4fdc9ce501c604f967
Anatoly Polivin <anatoly@kapkanov.com>
2024-11-07 23:13:56 +0400

    uppercase types

--- limits32.c
+++ limits32.c
@@ -4,42 +4,42 @@
 #include "includes/assert.c"
 #include "includes/types32.c"
 
-const i8  I8_MIN  = -128;
-const i8  I8_MAX  =  127;
-const u8  U8_MAX  =  255;
+const I8  I8_MIN  = -128;
+const I8  I8_MAX  =  127;
+const U8  U8_MAX  =  255;
 
-const i16 I16_MIN = -32768;
-const i16 I16_MAX =  32767;
-const u16 U16_MAX =  65535;
+const I16 I16_MIN = -32768;
+const I16 I16_MAX =  32767;
+const U16 U16_MAX =  65535;
 
-const i32 I32_MIN = -2147483648;
-const i32 I32_MAX =  2147483647;
-const u32 U32_MAX =  4294967295;
+const I32 I32_MIN = -2147483648;
+const I32 I32_MAX =  2147483647;
+const U32 U32_MAX =  4294967295;
 
 void assert_limits32(void) {
-  const u8 max8_unsigned = 0xff;
+  const U8 max8_unsigned = 0xff;
   assert(U8_MAX == max8_unsigned, "U8_MAX: expected %lu but got %lu\n", max8_unsigned, U8_MAX);
      
-  const i8 min8_signed = 0x80;
+  const I8 min8_signed = 0x80;
   assert(I8_MIN == min8_signed, "I8_MIN: expected %d but got %d\n", min8_signed, I8_MIN);
 
-  const i8 max8_signed = 0x7f;
+  const I8 max8_signed = 0x7f;
   assert(I8_MAX == max8_signed, "I8_MAX: expected %d but got %d\n", max8_signed, I8_MAX);
 
-  const u16 max16_unsigned = 0xffff;
+  const U16 max16_unsigned = 0xffff;
   assert(U16_MAX == max16_unsigned, "U16_MAX: expected %d but got %d\n", max16_unsigned, U16_MAX);
 
-  const i16 min16_signed = 0x8000;
+  const I16 min16_signed = 0x8000;
   assert(I16_MIN == min16_signed, "I16_MIN: expected %d but got %d\n", min16_signed, I16_MIN);
 
-  const i16 max16_signed = 0x7fff;
+  const I16 max16_signed = 0x7fff;
   assert(I16_MAX == max16_signed, "I16_MAX: expected %d but got %d\n", max16_signed, I16_MAX);
 
   /*
    * 2^32 - 1 = 2 * 2^31 - 1 = 2^31 - 1 + 2^31 = 2^30 * 2 - 1 + 2^30 * 2
    */
-  const u32 two_pow_thirty_unsigned = 1024 * 1024 * 1024;
-  u32       max32_unsigned;
+  const U32 two_pow_thirty_unsigned = 1024 * 1024 * 1024;
+  U32       max32_unsigned;
   max32_unsigned  = 2 * two_pow_thirty_unsigned;
   max32_unsigned -= 1;
   max32_unsigned += 2 * two_pow_thirty_unsigned;
@@ -49,8 +49,8 @@ void assert_limits32(void) {
   /*
    * 2^31 - 1 = 2 * 2^30 - 1 = 2^30 - 1 + 2^30
    */
-  const i32 two_pow_thirty_signed = 1024 * 1024 * 1024;
-  i32       max32_signed;
+  const I32 two_pow_thirty_signed = 1024 * 1024 * 1024;
+  I32       max32_signed;
   max32_signed  = two_pow_thirty_signed;
   max32_signed -= 1;
   max32_signed += two_pow_thirty_signed;
--- limits64.c
+++ limits64.c
@@ -4,17 +4,17 @@
 #include "includes/assert.c"
 #include "includes/types64.c"
 
-const i64 I64_MIN = 0x8000000000000000; /* -9223372036854775808 */
-const i64 I64_MAX = 0x7fffffffffffffff; /*  9223372036854775807 */
-const u64 U64_MAX = 0xffffffffffffffff; /* 18446744073709551615 */
+const I64 I64_MIN = 0x8000000000000000; /* -9223372036854775808 */
+const I64 I64_MAX = 0x7fffffffffffffff; /*  9223372036854775807 */
+const U64 U64_MAX = 0xffffffffffffffff; /* 18446744073709551615 */
 
 void assert_limits64(void) {
   /*
    * 2^64 - 1 = 2 * 2^63 - 1 = 2^63 - 1 + 2^63 = 2^60 * 8 - 1 + 2^60 * 8
    */
-  const u64 two_pow_thirty_unsigned = 1024 * 1024 * 1024;
-  const u64 two_pow_sixty_unsigned  = two_pow_thirty_unsigned * two_pow_thirty_unsigned;
-  u64       max64_unsigned;
+  const U64 two_pow_thirty_unsigned = 1024 * 1024 * 1024;
+  const U64 two_pow_sixty_unsigned  = two_pow_thirty_unsigned * two_pow_thirty_unsigned;
+  U64       max64_unsigned;
   max64_unsigned  = two_pow_sixty_unsigned;
   max64_unsigned  = 8 * two_pow_sixty_unsigned;
   max64_unsigned -= 1;
@@ -24,9 +24,9 @@ void assert_limits64(void) {
   /*
    * 2^63 - 1 = 8 * 2^60 - 1 = 4 * 2^60 - 1 + 4 * 2^60
    */
-  const i64 two_pow_thirty_signed = 1024 * 1024 * 1024;
-  const i64 two_pow_sixty_signed  = two_pow_thirty_signed * two_pow_thirty_signed;
-  i64       max64_signed;
+  const I64 two_pow_thirty_signed = 1024 * 1024 * 1024;
+  const I64 two_pow_sixty_signed  = two_pow_thirty_signed * two_pow_thirty_signed;
+  I64       max64_signed;
   max64_signed  = 4 * two_pow_sixty_signed;
   max64_signed -= 1;
   max64_signed += 4 * two_pow_sixty_signed;
